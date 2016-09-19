ac717e599bbbd2fb2389c22800809d6d5f7cfbb4
Wyatt McBain <wyatt.mcbain@zx-ventures.com>
2016-09-19 13:49:14 -0400

    Add CGRect center helper

---
+++ ZXFoundation/ZXFoundation/Foundation/CGRect.swift
@@ -0,0 +1,26 @@
+//
+//  CGRect.swift
+//  ZXFoundation
+//
+//  Created by Wyatt McBain on 9/19/16.
+//  Copyright © 2016 ZX Ventures. All rights reserved.
+//
+
+import Foundation
+
+extension CGRect {
+    
+    /**
+     Center point of the CGRect frame.
+     
+     Determined by the min and max values of the x and y axis.
+     
+     
+     ```
+     CGPoint(x: (maxX - minX) / 2, y: (maxY - minY) / 2)
+     ```
+     */
+    var center: CGPoint {
+        return CGPoint(x: (maxX - minX) / 2, y: (maxY - minY) / 2)
+    }
+}
